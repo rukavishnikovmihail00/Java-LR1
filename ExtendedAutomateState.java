72fa32754cd7b4b963b17577756006dce602331d
Mikhail Rukavishnikov <61332555+rukavishnikovmihail00@users.noreply.github.com>
2020-03-13 19:45:45 +0300

    Add files via upload

--- ExtendedAutomateState.java
+++ ExtendedAutomateState.java
@@ -3,29 +3,26 @@ package com.company;
 public class ExtendedAutomateState extends AutomateState {
     private int new_id1, new_id2, new_id3;
 
-    public ExtendedAutomateState(int id1, int id2, int id3, int new_id1, int new_id2, int new_id3) {
+    public ExtendedAutomateState(int id1, int id2, int id3) {
         super(id1, id2, id3);
-        this.new_id1 = new_id1;
-        this.new_id2 = new_id2;
-        this.new_id3 = new_id3;
-    }
-
-    public ExtendedAutomateState(int new_id1, int new_id2, int new_id3) {
-        this.new_id1 = new_id1;
-        this.new_id2 = new_id2;
-        this.new_id3 = new_id3;
     }
 
     public void changeState(){
-     //Здесь надо написать алгоритм изменения значений
+        new_id1 = super.id3;
+        new_id2 = super.id1;
+        new_id3 = super.id2;
     }
 
-    public void print(){
-        System.out.println(getId1());
-        System.out.println(getId2());
-        System.out.println(getId3());
+    public void printState()
+    {
+        System.out.println(id1);
+        System.out.println(id2);
+        System.out.println(id3);
         System.out.println(new_id1);
         System.out.println(new_id2);
         System.out.println(new_id3);
     }
+
+
+
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-// AutomateState - абстрактный класс, ExtendedAutomateState - его наследник
-// В классе ExtendetAutomateState нужно написать метод, осуществляющий измененение состояний, где
-// id1, id2, id3 - начальные состояния, а new_id1, new_id2, new_id3 - новые состояния
-
 package com.company;
 
 import java.util.Scanner;
@@ -17,8 +13,8 @@ public class Main {
         System.out.println("ID 3: ");
         int id3 = in.nextInt();
 
-        ExtendedAutomateState objEAS = new ExtendedAutomateState(id1, id2, id3);
-        objEAS.changeState();
-        objEAS.print();
+        ExtendedAutomateState oExtended = new ExtendedAutomateState(id1,id2,id3);
+        oExtended.changeState();
+        oExtended.printState();
     }
 }
